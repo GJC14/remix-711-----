fb6d3b17f47f706b34c12cac398a664d4fe03af4
gjc14 <yin@yinc.me>
2024-09-23 20:27:58 -0500

    Initial commit from create-remix

---
+++ app/routes/_index.tsx
@@ -0,0 +1,16 @@
+import type { MetaFunction } from "@remix-run/node";
+
+export const meta: MetaFunction = () => {
+  return [
+    { title: "New Remix App" },
+    { name: "description", content: "Welcome to Remix!" },
+  ];
+};
+
+export default function Index() {
+  return (
+    <div className="font-sans p-4">
+      <h1 className="text-3xl">歡迎使用統一超商門市選擇器</h1>
+    </div>
+  );
+}
